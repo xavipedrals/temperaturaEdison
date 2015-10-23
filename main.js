bdc00f68549f7ef19a9b46ec953d9a1bfe5bf1f7
Xavi <xavi@Xavis-MacBook-Air.local>
2015-10-23 22:59:52 +0200

    Codi netejat i commentat

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-var express = require('express');
-
-var mraa = require('mraa'); //require mraa
-var fs = require('fs');
-var express = require('express');
-var http = require('http');
-var request = require('request');
-var bodyParser = require('body-parser');
-
+var mraa = require('mraa'); //es la llibreria per interactuar amb els pins d'Arduino
+var fs = require('fs'); //serveix per llegir fitxers escrits en Arduino, en aquest projecte no el faig servir tot i que l'incloc
+var express = require('express'); //és el servidor en si
+var http = require('http'); //és obligatori
+var request = require('request'); //és per fer peticions al servidor on es guarden les dades
+var bodyParser = require('body-parser'); //és per llegir dades en JSON adjuntes en un petició entrant
 
 console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
-var myAnalogPin = new mraa.Aio(0);
+var myAnalogPin = new mraa.Aio(0);  //pin sensor temperatura
 
+//pins motors
 var motorpin1 = new mraa.Gpio(8);
 var motorpin2 = new mraa.Gpio(9);
 var motorpin3 = new mraa.Gpio(10);
@@ -21,34 +19,37 @@ motorpin2.mode(mraa.DIR_OUT);
 motorpin3.mode(mraa.DIR_OUT);
 motorpin4.mode(mraa.DIR_OUT);
 
+//constants
+var B = 3975; //Aquesta serveix per calcular la temperatura
 var delayTime = 5;
 var ledTouch = 6;
 var touch=5;
-
 var tMax = 26;
 var tMin = 20;
 
+//variables globals
 var estaPujant = false;
-
 var temperatura = 0;
 
 var app = express();
 
 app.use(bodyParser.json());
 
+//endpoint temperatura
 app.use('/temperatura',  function(req, res){
   res.status(200).send({temperatura: temperatura});
 });
 
+//endpoint motors
+//serveix per fer girar els motors amunt o avall
 app.use('/motors', function(req, res){
-  var moviment = req.body.moviment;
+  var moviment = req.body.moviment; //agafa el JSON adjunt a la petició i grada en vriable
   console.log("moviment " + moviment);
   var steps = 1200;
   var i = 0;
 
   if (moviment === 0){ //pujar
     function doARoll(n){
-      // console.log("Loop " + n);
       setTimeout(function(){
         motorpin1.write(1);
         motorpin2.write(0);
@@ -77,11 +78,10 @@ app.use('/motors', function(req, res){
       },delayTime);
     }
     doARoll(steps);
-    res.status(200).send("Amuuuuunt");
+    res.status(200).send("Amuuuuunt"); //Respon al servidor
   }
   else {
-    function doARoll2(n){
-      // console.log("Loop " + n);
+    function doARoll2(n){ //baixar
       setTimeout(function(){
         motorpin1.write(0);
         motorpin2.write(0);
@@ -110,13 +110,9 @@ app.use('/motors', function(req, res){
       },delayTime);
     }
     doARoll2(steps);
-    res.status(200).send("Avaaaall");
+    res.status(200).send("Avaaaall"); //Respon al servidor
   }
-
-
 });
-
-console.log("Hola champion");
 
 // We need this to build our post string
 var querystring = require('querystring');
@@ -125,32 +121,21 @@ var stillPosting = false;
 var url = "https://tranquil-sea-4354.herokuapp.com/temperatura";
 
 
-// var temperatura = 0;
-// var tMax = 30;
-// var tMin = 24;
-var B = 3975;
-
+//Aquest codi s'executa periòdicament (loop)
 setInterval(function () {
   var a = myAnalogPin.read();
   console.log("Analog Pin (A0) Output: " + a);
   var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
-  //console.log("Resistance: "+resistance);
   temperatura = 1 / (Math.log(resistance / 10000) / B + 1 / 298.15) - 273.15;
+  console.log("Celsius Temperature "+temperatura);
 
-  if(temperatura > 26 && !stillPosting){
+  if(temperatura > tMax && !stillPosting){  //si la temperatura supera el màxim avisa al servidor
     stillPosting = true;
-    //inici request
-    var data = {
-      temperatura: temperatura
-    };
-    console.log(data.temperatura);
-
-    request(
-    {
+    //inici http request
+    request({
       url: url,
       method: "POST",
       json: data
-
     }, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         console.log(body)
@@ -162,13 +147,14 @@ setInterval(function () {
       }
       stillPosting = false;
     });
-    //CODI GUARROOOOOO
+    //fi http request
+    //CODI GUARROOOOOO per fer moure automàticament el motor si temperatura > tMax sense esperar una ordre del servidor
+    //NOTA: És un copy paste de més amunt
     var steps = 600;
     var i = 0;
-    if(!estaPujant){
+    if(!estaPujant){  //variable que controla si el motor ja esta pujant
       estaPujant = true;
-      function doARoll(n){
-        // console.log("Loop " + n);
+      function doARoll3(n){
         setTimeout(function(){
           motorpin1.write(1);
           motorpin2.write(0);
@@ -189,53 +175,18 @@ setInterval(function () {
                 motorpin2.write(0);
                 motorpin3.write(0);
                 motorpin4.write(1);
-                if(n > 0)doARoll(n-1);
+                if(n > 0)doARoll3(n-1);
 
               },delayTime);
             },delayTime);
           },delayTime);
         },delayTime);
       }
-      doARoll(steps);
+      doARoll3(steps);
     }
     estaPujant = false;
-
-    // function doARoll(n){
-    //   // console.log("Loop " + n);
-    //   setTimeout(function(){
-    //     motorpin1.write(1);
-    //     motorpin2.write(0);
-    //     motorpin3.write(0);
-    //     motorpin4.write(0);
-    //     setTimeout(function(){
-    //       motorpin1.write(0);
-    //       motorpin2.write(1);
-    //       motorpin3.write(0);
-    //       motorpin4.write(0);
-    //       setTimeout(function(){
-    //         motorpin1.write(0);
-    //         motorpin2.write(0);
-    //         motorpin3.write(1);
-    //         motorpin4.write(0);
-    //         setTimeout(function(){
-    //           motorpin1.write(0);
-    //           motorpin2.write(0);
-    //           motorpin3.write(0);
-    //           motorpin4.write(1);
-    //           if(n > 0)doARoll(n-1);
-    //
-    //         },delayTime);
-    //       },delayTime);
-    //     },delayTime);
-    //   },delayTime);
-    // }
-    // doARoll(steps);
-
-
-    //dasdsdsas
   }
-  console.log("Celsius Temperature "+temperatura);
 }, 5000);
 
-
+//això és la linia que realment crea el servidor en base la variable app (té els endpoints)
 http.createServer(app).listen(8080);
--- routes/user.js
+++
@@ -1,8 +0,0 @@
-var userRouter = require('express').Router();
-
-userRouter.get('/', function (req, res) {
-    console.log("Oeeeeeee!!");
-    res.status(200).send("hola champion");
-});
-
-module.exports = userRouter;
